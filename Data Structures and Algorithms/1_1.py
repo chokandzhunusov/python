8a8ba6427560104b934fa55c3233c2e8748f532b
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-05-01 09:33:22 +0600

    Change: code style

--- Data Structures and Algorithms/1_1.py
+++ Data Structures and Algorithms/1_1.py
@@ -2,7 +2,6 @@
 *************************************************
 **** Unpacking a Sequence into Separate variables
 *************************************************
-'''
 
 
 #########
@@ -17,10 +16,11 @@
 #########
 # Any sequence (or iterable) can be unpacked into variables using a simple assignment operation.    # noqa
 # The only requirement is that the number of variables and structure match the sequence.    # noqa
+'''
 
 # For example:
-# >>> p = (4, 5)
-# >>> x, y = p
+p = (4, 5)
+x, y = p
 
 data = ['ACME', 50, 91.1, (2012, 12, 21)]
 # TODO
