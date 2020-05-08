248e7246d82c3a2403e26d33fce1d17019246424
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-05-08 09:34:36 +0600

    Add: final version of priority queue implementation

--- Data Structures and Algorithms/1_5.py
+++ Data Structures and Algorithms/1_5.py
@@ -21,20 +21,26 @@ import heapq
 
 class PriorityQueue:
     def __init__(self):
-        self._queue = [] self._index = 0
+        self._queue = []
+        self._index = 0
 
     def push(self, item, priority):
-        heapq.heappush(self._queue, (-priority, self._index, item)) self._index += 1
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
 
     def pop(self):
         return heapq.heappop(self._queue)[-1]
 
 
 # TODO
-# Create an `Item` class which initially takes a name as param
+# Create an `Item` class which initially takes a `name` as param
 # and should return object representation
 
 
 # TODO
 # Create an instance of `PriorityQueue` class above
 # Push to it 4 'Item' objects
+
+
+if __name__ == '__main__':
+    pass
