d1c9532e97c5c08bfd6922f32ac30f22f4e4feeb
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-05-11 09:31:04 +0600

    Add: problem for ordered dictionaries

---
+++ Data Structures and Algorithms/1_7.py
@@ -0,0 +1,32 @@
+"""
+************************************************************
+**** 1.7. Keeping Dictionaries in Order Problem
+************************************************************
+
+
+#########
+# Problem
+#########
+# You want to create a dictionary,
+# and you also want to control the order of items when iterating or serializing.
+
+
+#########
+# Solution
+#########
+# To control the order of items in a dictionary,
+# you can use an OrderedDict from the collections module.
+# It exactly preserves the original insertion order of data when iterating.
+"""
+
+# For example
+
+from collections import OrderedDict
+
+# TODO:
+# Initialize an ordered dict
+# Add items to `dict` above
+
+
+if __name__ == '__main__':
+    pass
