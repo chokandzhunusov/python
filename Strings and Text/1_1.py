a4c50e2409db6ba57ef2dae7703ac673a5d0cbb9
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-05 09:48:41 +0600

    Create, Add: create new folder for examples on strings and texts add eg splitting string with multiple delimeters

---
+++ Strings and Text/1_1.py
@@ -0,0 +1,50 @@
+"""
+************************************************************
+**** 1.1.  Splitting Strings on Any of Multiple Delimiters
+************************************************************
+
+
+#########
+# Problem
+#########
+# You need to split a string into fields,
+# but the delimiters (and spacing around them)
+# aren’t consistent throughout the string.
+
+
+#########
+# Solution
+#########
+# The `split()` method of string objects is really meant
+# for very simple cases, and does not allow for multiple delimiters
+# or account for possible whitespace around the delimiters.
+# In cases when you need a bit more flexibility, use the `re.split()` method
+"""
+
+import re
+
+
+line = 'asdf fjdk; afed, fjek,asdf, foo'
+
+# TODO
+# Split the variable `line` above by:
+# semicolumn (`;`) comma (`,`)  and spaces (` `) using `re`
+
+
+# TODO
+# Split the variable `line` into var `fields` but this time
+# include the matched results
+
+
+# TODO
+# Extract `values` from `fields` you created above
+# Extract `delimeters` from `fields` you created above(TRICKY
+# when will try to recreate `line`)
+
+
+# TODO
+# Now recreate the var `line` from `values` and `delimeters`
+
+
+if __name__ == '__main__':
+    pass
