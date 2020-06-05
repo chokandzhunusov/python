435a0f0141a54edc67e3a0ad0059539285b00357
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-05 10:25:30 +0600

    Add: best practice for matching string at `begin`/`end`

---
+++ Strings and Text/1_2.py
@@ -0,0 +1,39 @@
+"""
+************************************************************
+**** 1.1.  Matching Text at the Start or End of a String
+************************************************************
+
+
+#########
+# Problem
+#########
+# You need to check the start or end of a string for specific text patterns,
+# such as filename extensions, URL schemes, and so on.
+
+
+#########
+# Solution
+#########
+# A simple way to check the beginning or end of a string is to use the
+# `str.starts with()` or `str.endswith()` methods.
+"""
+
+
+import os
+
+
+# TODO
+# Get the files form ../Data Structures...
+# Extract those ones that starts with `1_2`
+# Check if there is any file that starts with `5`
+
+
+choices = ['http:', 'ftp:']
+url = 'http://www.python.org'
+# TODO
+# Try to give `choices` to `url.startswith()`
+# Now do it in right way
+
+
+if __name__ == '__main__':
+    pass
