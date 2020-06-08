7217145a0bee0422352c25aa13d445a2709de3d3
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-08 10:17:42 +0600

    Add: match in string using `regex`

--- Strings and Text/1_4.py
+++ Strings and Text/1_4.py
@@ -34,7 +34,48 @@ text = 'yeah, but no, but yeah, but no, but yeah'
 text1 = '11/27/2012'
 text2 = 'Nov 27, 2012'
 # TODO
-# Write an `if/else` clause that determines `true/false` of match using `re`
+# Write an `if/else` clause that determines `true/false` of `text1`
+# and `text2` match using `re`
+
+
+# TODO
+# Precompile the regular expression pattern into a pattern object first
+# DO the previous `TODO` with compiled version
+
+
+text3 = 'Today is 11/27/2012. PyCon starts 3/13/2013.'
+# TODO
+# `match` searches only starts with occurrence in the string
+# but you need to FIND ALL matches in text, find them
+
+
+# INFO
+# When defining regular expressions, it is common to introduce `capture groups`
+# by enclosing parts of the pattern in parentheses.
+
+
+# TODO
+# Compile `\d+/\d+/\d+` pattern with capture groups
+# Print out `groups`(from 0, 3)
+# Extract them to `month, day, year`
+# Find all matches from `text3` using pattern with compiled groups
+# Loop over them and print items in format: `month-day-year`
+
+
+# TODO
+# Loop over matches found using `findall`,
+# but this time iteratively using `finditer()`
+
+
+# INFO
+# If you want an exact match,
+# make sure the pattern includes the end-marker `$`
+
+
+text4 = '12/23/2020awdaw'
+# TODO
+# Try to find exact match of date in `text4`
+
 
 if __name__ == '__main__':
     pass
