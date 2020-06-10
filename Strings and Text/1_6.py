7df2e7e4abf99d1a57e779f7c8c01e754b9d440b
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-10 09:40:04 +0600

    Add: approach of searching and replacing string in a case-insensitive mode

---
+++ Strings and Text/1_6.py
@@ -0,0 +1,33 @@
+"""
+************************************************************
+**** 1.6. Searching and Replacing Case-Insensitive Text
+************************************************************
+
+
+#########
+# Problem
+#########
+# You need to search for and possibly replace text in a case-insensitive manner.
+
+
+#########
+# Solution
+#########
+# To perform case-insensitive text operations,
+# you need to use the re module and supply
+# the `re.IGNORECASE flag` to various operations
+"""
+
+
+import re
+
+
+text = 'UPPER PYTHON, lower python, Mixed Python'
+# TODO
+# Find all match cases in text with case-insensitive approach
+# Find all match cases in text with case-insensitive approach
+# and replace them with word 'bound'
+
+
+if __name__ == '__main__':
+    pass
