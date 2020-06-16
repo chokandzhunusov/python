f9290d8e9913833e84b8d7d3842e676a312698eb
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-16 08:49:17 +0600

    Add: info about handling weird chars in string

---
+++ Strings and Text/1_12.py
@@ -0,0 +1,30 @@
+"""
+************************************************************
+**** 1.12. Sanitizing and Cleaning Up Text
+************************************************************
+
+
+#########
+# Problem
+#########
+# Some bored script kiddie has entered the text “pýtĥöñ”
+# into a form on your web page and you’d like to clean it up somehow.
+
+
+#########
+# Solution
+#########
+# Throught examples
+"""
+
+
+# INFO
+# Needs to be researched before apply any further depth actions
+# But in general, what I've tried IDE or python interpreter
+# auto handles such cases. Not every time, but generally.
+
+s = 'pýtĥöñ\fis\tawesome\r\n'
+
+
+if __name__ == '__main__':
+    pass
