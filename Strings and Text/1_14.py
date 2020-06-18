bfa45578fbfa76596420fd7a39c8660b90303531
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-18 10:20:58 +0600

    Add: combining small strings together into one large

---
+++ Strings and Text/1_14.py
@@ -0,0 +1,37 @@
+"""
+************************************************************
+**** 1.14. Combining and Concatenating Strings
+************************************************************
+
+
+#########
+# Problem
+#########
+# You want to combine many small strings together into a larger string.
+
+
+#########
+# Solution
+#########
+# If the strings you wish to combine are found in a sequence or iterable,
+# the fastest way to combine them is to use the `join()` method.
+"""
+
+parts = ['Is', 'Chicago', 'Not', 'Chicago?']
+# TODO
+# Join parts by ' '(space); ','(comma); ''(no space)
+
+
+data = ['ACME', 50, 91.1]
+# TODO
+# Join them by ', '(comma)
+
+
+a, b, c = 'Dzhunusov', 'Chokan', 'Zaurbekovich'
+# TODO
+# Print the variables above by separating them with ':',
+# the idea is to show lookouts for unnecessary string concatenations.
+
+
+if __name__ == '__main__':
+    pass
