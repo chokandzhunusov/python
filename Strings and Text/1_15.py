b1b97bb271aadad21a8fbe9a725a8d2cfb55fe1d
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-18 10:36:31 +0600

    Add: combining and concatenating strings using `format()`, `format_map()` methods

---
+++ Strings and Text/1_15.py
@@ -0,0 +1,34 @@
+"""
+************************************************************
+**** 1.14. Combining and Concatenating Strings
+************************************************************
+
+
+#########
+# Problem
+#########
+# Interpolating Variables in Strings.
+
+
+#########
+# Solution
+#########
+# Python has no direct support for simply substituting variable values in strings.
+# However, this feature can be approximated using the `format()` method of strings.
+"""
+
+
+s = '{name} has {n} messages.'
+# TODO
+# Using `format()` method add `name` and `n` to `s`
+
+
+name = 'Chokan'
+n = 2
+# TODO
+# Using `format_map()` and `vars()` methods print
+# `s` with corresponding variables.
+
+
+if __name__ == '__main__':
+    print(vars())
