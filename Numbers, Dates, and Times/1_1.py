a4a45e9ee74f7fa422e23de3a1964ec6340863ac
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-07-07 09:40:51 +0600

    Add: eg of rounding values

---
+++ Numbers, Dates, and Times/1_1.py
@@ -0,0 +1,44 @@
+"""
+************************************************************
+**** 1.1. Rounding Numerical Values
+************************************************************
+
+
+#########
+# Problem
+#########
+# You want to round a floating-point number to a fixed number
+# of decimal places.
+
+
+#########
+# Solution
+#########
+# For simple rounding, use the built-in `round(value, ndigits)` function.
+"""
+
+# TODO
+# Round till decimals
+a = 1.23
+b = 1.27
+c = -1.27
+
+# Round till thousands
+d = 1.25361
+
+
+# TODO
+# What is the expected behavior of 1.5 or 2.5 if it'll be rounded
+
+
+# TODO
+# Try to use negative digit to round `value`
+k = 1627731
+
+
+# TODO
+x = 1.23456
+# Format `x` for output with 2 decimal places
+
+if __name__ == '__main__':
+    pass
