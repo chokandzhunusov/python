0a7d86f44514fa82698e488f4cb5b896525be237
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-09-02 14:45:47 +0600

    ADD: formatting number for output

---
+++ Numbers, Dates, and Times/1_3.py
@@ -0,0 +1,30 @@
+"""
+************************************************************
+**** 1.3. Formatting Numbers for Output
+************************************************************
+
+
+#########
+# Problem
+#########
+# You need to format a number for output,
+# controlling the number of digits, alignment,
+# inclusion of a thousands separator, and other details.
+
+#########
+# Solution
+#########
+# To format a single number for output,
+# use the built-in `format()` function.
+"""
+
+
+x = 1234.56789
+# TODO
+# Format `x` for two decimal places of accuracy. Try to do using  `%` operator.
+# Format `x` to right/left/centered justified in 10 chars, one-digit accuracy.
+# Include thousands separator to `x`.
+
+
+if __name__ == '__main__':
+    pass
