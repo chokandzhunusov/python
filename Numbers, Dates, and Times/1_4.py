8825f01780a622fb5ec4b99dca35ec44d87fe8a9
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-10 14:37:48 +0600

    ADD: convertion values into hex, oct, byte representations

---
+++ Numbers, Dates, and Times/1_4.py
@@ -0,0 +1,34 @@
+"""
+************************************************************
+**** 3.4. Working with Binary, Octal, and Hexadecimal Integers
+************************************************************
+
+
+#########
+# Problem
+#########
+# You need to convert or output integers represented by binary, octal, or hexadecimal digits.
+
+
+#########
+# Solution
+#########
+# Use `bin()`, `oct()`, `hex()` or  `format()` function.
+"""
+
+
+x = 1234
+# TODO
+# Convert `x` to binary, octal, hexadecimal format.
+# Format `x` to binary, octal, hexadecimal format using `format()` to remove prefix from above methods.
+
+# NOTE
+# Integers are signed. Means if you will convert negative ones output will be negative.
+
+# TODO
+# Convert `10011010010` to integer value.
+# Convert `4d2` to integer value.
+
+
+if __name__ == '__main__':
+    pass
