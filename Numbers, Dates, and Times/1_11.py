ca3266fc2375cb3aa7ed0c148902c581844e6d18
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-12 15:27:22 +0600

    ADD: working with `random` module

---
+++ Numbers, Dates, and Times/1_11.py
@@ -0,0 +1,34 @@
+"""
+************************************
+**** 3.11. Picking Things at Random
+************************************
+
+
+#########
+# Problem
+#########
+# You want to pick random items out of a sequence or generate random numbers.
+
+
+#########
+# Solution
+#########
+# The `random` module has various functions for random numbers and picking random items.
+"""
+
+
+import random
+
+
+values = [1, 2, 3, 4, 5, 6, 7]
+
+# TODO
+# Choose a random item from `values` above. Use choice()
+# Retrieve 2 or 3 random items from `values`. Use sample()
+# Mix items of `values` list
+# Produce random int from 1 to 10.
+# produce some floating num from 0 to 1.
+
+
+if __name__ == '__main__':
+    pass
