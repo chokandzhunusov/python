4e209a780a4ce424d6dbabbf843cf972138abfe8
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-17 15:18:41 +0600

    ADD: problem of using iterator without `for` loop and taking control of the iteration flow

---
+++ Iterators and Generators/1_1.py
@@ -0,0 +1,31 @@
+"""
+**********************************************
+**** 4.1. Manually Consuming an Iterator
+**********************************************
+
+
+#########
+# Problem
+#########
+# You need to process items in an iterable, but for whatever reason,
+# you can’t or don’t want to use a for loop.
+
+
+
+#########
+# Solution
+#########
+# To manually consume an iterable, use the `next()` function and
+# write your code to catch the StopIteration exception.
+"""
+
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+# TODO
+# Iterate over `nums` till 3rd index above but do not use for loop. And catch StopIteration exception
+
+
+if __name__ == '__main__':
+    pass
