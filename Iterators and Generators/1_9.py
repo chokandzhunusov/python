2c51244f4a04d3438d839c96e00ef0899c34c1bb
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-26 15:48:16 +0600

    ADD: aplying permutation and cobination over items

---
+++ Iterators and Generators/1_9.py
@@ -0,0 +1,37 @@
+"""
+******************************************************************
+**** 4.9. Iterating Over All Possible Combinations or Permutations
+******************************************************************
+
+#########
+# Problem
+#########
+# You want to iterate over all of the possible combinations or
+# permutations of a collection of items.
+
+#########
+# Solution
+#########
+# The itertools module provides three functions for this task.
+# The first of these—iter tools.permutations()—takes a collection of items
+# and produces a sequence of tuples that rearranges all of the items into
+# all possible permutations (i.e., it shuffles them into all possible configurations).
+
+"""
+
+
+import itertools
+
+
+items = ['a', 'b', 'c']
+# TODO
+# print all possible permutations of items
+# reduce length of permutation
+
+
+# TODO
+# Use itertools.combinations() to produce a sequence of combinations
+# of items.
+
+if __name__ == '__main__':
+    pass
