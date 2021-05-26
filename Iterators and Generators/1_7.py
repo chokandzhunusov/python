8b555ca128dd0ef25e78a63015db3cdc0bd9b1e3
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-26 15:22:31 +0600

    ADD: using slice in iterator

---
+++ Iterators and Generators/1_7.py
@@ -0,0 +1,34 @@
+"""
+***************************************
+**** 4.7. Taking a Slice of an Iterator
+***************************************
+
+#########
+# Problem
+#########
+# You want to take a slice of data produced by an iterator,
+# but the normal slicing operator doesn’t work.
+
+#########
+# Solution
+#########
+# The itertools.islice() function is perfectly suited
+# for taking slices of iterators and generators.
+
+"""
+
+
+import itertools
+
+
+# TODO
+# Write a generator function that will take `n` as arg and increment it.
+
+def count(n):
+    pass
+
+
+if __name__ == '__main__':
+    # Code goes here
+    # print slice 10:20 using `loop` and `islice` method
+    pass
