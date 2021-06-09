9c9ecbf3e93ec2be3810db0df35284566dee4868
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-06-09 16:08:56 +0600

    WIP: read/write txt file

---
+++ Files and I:O/1_1.py
@@ -0,0 +1,38 @@
+"""
+***************************************
+**** 5.1. Reading and Writing Text Data
+***************************************
+
+#########
+# Problem
+#########
+    - You need to read or write text data,
+      possibly in different text encodings such as ASCII, UTF-8, or UTF-16.
+
+#########
+# Solution
+#########
+    - Use the `open()` function with mode `rt` to read a text file.
+    - Use `open()` with mode `wt` to write a text file.
+
+# TODO
+#   - Read the entire file as a single string.
+    - Iterate over the lines of the file.
+    - Write chunks of text data.
+"""
+
+from collections import Iterable
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
+
+
+
+
+
