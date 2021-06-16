06aeae794b1fd8811d55c4931ebb3249061b3986
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-06-16 16:07:58 +0600

    ADD: `print()` with diff. separator or line ending

---
+++ Files and I:O/1_3.py
@@ -0,0 +1,41 @@
+"""
+*************************************************************
+**** 5.3. Printing with a Different Separator or Line Ending
+*************************************************************
+
+#########
+# Problem
+#########
+    - You want to output data using print(),
+      but you also want to change the separator character or line ending.
+
+#########
+# Solution
+#########
+    - Use the `sep` and `end` keyword arguments to print(),
+      to change the output as you wish.
+
+# TODO:
+    - Print it by separating comma: 'ACME', 50, 91.5
+    - "                           " and by adding `!!\n` at the end.
+    - Print nums in range(5) one by one and in one line.
+    - Try to print `join` of `row` below and face an error.
+    - Try to print `row` below by using `join` and separator comma(,)
+    - Now print `row` using `sep` key of `print`.
+
+"""
+
+row = ('ACME', 50, 91.5)
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
+
+
+
+
+
