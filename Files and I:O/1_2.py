7bb54017b4d2ff78719832c4ca0411ae66bcde64
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-06-16 15:41:27 +0600

    ADD: catching output of print and writing it to file

---
+++ Files and I:O/1_2.py
@@ -0,0 +1,35 @@
+"""
+****************************
+**** 5.2. Printing to a File
+****************************
+
+#########
+# Problem
+#########
+    - You want to redirect the output of the print() function to a file.
+
+#########
+# Solution
+#########
+    - Use the `file` keyword argument to print()
+
+# NOTE:
+    - File should be opened in `text` mode not in `binary`
+
+# TODO:
+    - Open file and write them to file2 by redirecting print() output
+"""
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
+
+
+
+
+
