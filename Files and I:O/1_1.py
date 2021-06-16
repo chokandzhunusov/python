877e8e6986f282369b0186843abb0d8ad36c9f9d
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-06-16 15:31:44 +0600

    ADD: manipulation of read/write file

--- Files and I:O/1_1.py
+++ Files and I:O/1_1.py
@@ -15,10 +15,17 @@
     - Use the `open()` function with mode `rt` to read a text file.
     - Use `open()` with mode `wt` to write a text file.
 
-# TODO
-#   - Read the entire file as a single string.
+# NOTE:
+    - Opening file using `with` auto closes file,
+      if it was not used, do not forget to `close()`
+
+# TODO:
+    - Read the entire file as a single string.
     - Iterate over the lines of the file.
     - Write chunks of text data.
+    - Try to open file.txt in `ascii` mode and read
+    - File `file.txt` contains line `Spicy Jalapeño`, which contains non `ascii` char
+      try to read it by replacing or ingnoring error causing char.
 """
 
 from collections import Iterable
