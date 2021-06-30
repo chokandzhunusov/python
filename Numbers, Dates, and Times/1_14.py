377df6ca15110bd1feb5242f92d1c09bb37e063e
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-06-30 15:37:27 +0600

    CHANGE: some instructions

--- Numbers, Dates, and Times/1_14.py
+++ Numbers, Dates, and Times/1_14.py
@@ -24,21 +24,22 @@ import calendar
 
 from datetime import datetime, date, timedelta
 
+
 def get_month_range(start_date=None):
     if start_date is None:
         # Code goes here
-        # Initialize start_date with first day of the month
+        # Initialize start_date with first day of current month
         pass
     _, days_in_month = calendar.monthrange(start_date.year, start_date.month)
     # Code goes here
     # Define end_date use timedelta
-    return (start_date, end_date)
+    return start_date, end_date
 
 
 def print_days_of_month():
     # Code goes here
     # Initialize `a_day` with day=1
-    a_day = ''
+    a_day = None
     first_day, last_day = get_month_range()
     while first_day < last_day:
         print(first_day)
@@ -56,4 +57,5 @@ for d in date_range(datetime(2012, 9, 1), datetime(2012,10,1), timedelta(hours=6
 
 
 if __name__ == '__main__':
+    print_days_of_month()
     pass
