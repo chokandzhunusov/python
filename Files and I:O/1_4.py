04e6126898bf59de51e05d40288b6eefb21743a1
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-07-14 16:08:26 +0600

    ADD: reading and writing Binary Data

---
+++ Files and I:O/1_4.py
@@ -0,0 +1,31 @@
+"""
+*****************************************
+**** 5.4. Reading and Writing Binary Data
+*****************************************
+
+#########
+# Problem
+#########
+#   You need to read or write binary data,
+    such as that found in images, sound files, and so on.
+
+#########
+# Solution
+#########
+#   Use the open() function with mode `rb` or `wb` to read or write binary data.
+
+"""
+
+
+# TODO
+#   Write smhtng to binary(.bin) file
+#   Read that file file
+
+
+# TODO
+#   Write to file as above but encode properly to `utf-8`
+#   Read it, and node how is output of file and text differs
+
+
+if __name__ == '__main__':
+    pass
