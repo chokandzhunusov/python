bcff0fc4045fdbb66d416b565a79ac264ce9cd8f
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-07-19 15:45:03 +0600

    ADD: operation that can be done when you wnat to treat string as file-like object

---
+++ Files and I:O/1_6.py
@@ -0,0 +1,28 @@
+"""
+***********************************************
+**** 5.6. Performing I/O Operations on a String
+***********************************************
+
+#########
+# Problem
+#########
+#   You want to feed a text or binary string to code
+    that’s been written to operate on file-like objects instead.
+
+#########
+# Solution
+#########
+#   Use the io.StringIO() and io.BytesIO() classes
+    to create file-like objects that operate on string data.
+
+"""
+
+
+# TODO
+#   Instantiate a file-like object.
+#   Write some text to it.
+#   Try to get values using method: getvalue()
+
+
+if __name__ == '__main__':
+    pass
