7f74312d8589e353faf3f4bec7969c331cb77d4d
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-07-19 15:36:51 +0600

    ADD: writing to file, but first check if it is already exists

---
+++ Files and I:O/1_5.py
@@ -0,0 +1,27 @@
+"""
+******************************************************
+**** 5.5. Writing to a File That Doesn’t Already Exist
+******************************************************
+
+#########
+# Problem
+#########
+#   You want to write data to a file,
+    but only if it doesn’t already exist on the filesystem.
+
+#########
+# Solution
+#########
+#   This problem is easily solved by using the little-known
+    `x` mode to open() instead of the usual w mode.
+
+"""
+
+
+# TODO
+#   Write smhtng to file
+#   Try to write smthg to same file, but with respect if it not exists
+
+
+if __name__ == '__main__':
+    pass
