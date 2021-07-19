f333266140c36c7c0d319028978efed02f6f5b1d
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-07-19 15:55:36 +0600

    ADD: reading and writing compressed files

---
+++ Files and I:O/1_7.py
@@ -0,0 +1,30 @@
+"""
+**************************************************
+**** 5.7. Reading and Writing Compressed Datafiles
+**************************************************
+
+#########
+# Problem
+#########
+#   You need to read or write data in a file with gzip or bz2 compression.
+
+#########
+# Solution
+#########
+#   The gzip and bz2 modules make it easy to work with such files.
+    Both modules provide an alternative implementation of open()
+    that can be used for this purpose.
+
+"""
+
+
+import gzip
+
+
+# TODO
+#   Using gzip.open() some file.gz in write mode, try to write some text.
+#   Read that file
+
+
+if __name__ == '__main__':
+    pass
